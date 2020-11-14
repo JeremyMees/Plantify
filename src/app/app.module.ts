1db7aae71a0ebce01bc315218152e3703cc05ccd
JeremyMees <jeremymees123@gmail.com>
2020-11-14 09:12:18 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+import { ContainerComponent } from './container/container.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    DetailsComponent,
+    ContainerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
