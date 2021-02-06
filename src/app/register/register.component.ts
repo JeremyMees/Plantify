fa769b74ecceac7d381a1d3d1505a511a31d3c2d
Jeremy Mees <jeremymees123@gmail.com>
2021-02-06 09:16:48 +0100

    updated email verification

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -31,9 +31,10 @@ export class RegisterComponent {
   }
 
   checkEmail(event: any, email: string): void {
-    email.includes('@') === false || email.includes('.') === false
-      ? (this.colorEmail = 'red')
-      : (this.colorEmail = 'green');
+    const emailRegex: RegExp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    emailRegex.test(email)
+      ? (this.colorEmail = 'green')
+      : (this.colorEmail = 'red');
   }
 
   checkName(event: any, name: string): void {
@@ -48,7 +49,8 @@ export class RegisterComponent {
     password: string,
     passwordc: string
   ): void {
-    if (email.includes('@') === false || email.includes('.') === false) {
+    const emailRegex: RegExp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    if (emailRegex.test(email) === false) {
       this.notificationService.setNotification(
         'Please enter a valid email address',
         'bottom',
