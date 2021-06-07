a454e8869d911f514a48b6a7a162cb7eaaf7dca1
Jeremy Mees <jeremymees123@gmail.com>
2021-06-07 21:16:05 +0200

    Merged with master

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { AnimationOptions } from 'ngx-lottie';
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.scss']
+  styleUrls: ['./page-not-found.component.scss'],
 })
-export class PageNotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PageNotFoundComponent {
+  notFound: AnimationOptions = {
+    path: '../../assets/404.json',
+  };
 }
